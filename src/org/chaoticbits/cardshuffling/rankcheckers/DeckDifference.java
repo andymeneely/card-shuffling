a8de74483b279b115191dfb96441d3b7551b94ab
apmeneel <andy.meneely@gmail.com>
2011-04-17 13:05:57 -0400

    now we have a bridge hand compare

--- src/org/chaoticbits/cardshuffling/rankcheckers/DeckDifference.java
+++ src/org/chaoticbits/cardshuffling/rankcheckers/DeckDifference.java
@@ -6,6 +6,11 @@ import org.chaoticbits.cardshuffling.cards.PlayingCard;
 
 public class DeckDifference implements IRankChecker {
 
+	/**
+	 * Compares the two decks slot by slot to see how similar or different they are. Returns the number of
+	 * cards different in each slot. Output ranges from 0 to 52. 
+	 * 
+	 */
 	@Override
 	public Double compareRanks(List<PlayingCard> before, List<PlayingCard> after) {
 		if (before.size() != 52 && after.size() != 52)
